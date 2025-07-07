0d807d9cf3553b14d33545d26951a4404645009d
rasprogrammer <rasprogrammer@gmail.com>
2025-07-07 13:46:51 +0530

    Adding SpinLoader

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,22 @@
 import "./style/app.css";
 
+import { Suspense, lazy } from "react";
+import { BrowserRouter } from "react-router-dom";
+import PageLoader from "@/components/PageLoader";
+
 function App() {
-  return <>RAS-Track</>;
+  const RastrackOS = lazy(() => import("./apps/RastrackOS"));
+
+  return (
+    <>
+      <BrowserRouter>
+        <h2>RAS-Track</h2>
+        <Suspense fallback={<PageLoader></PageLoader>}>
+          <RastrackOS />
+        </Suspense>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/PageLoader/index.jsx
@@ -0,0 +1,16 @@
+import { Spin } from "antd";
+
+import { LoadingOutlined } from "@ant-design/icons";
+
+function PageLoader() {
+  const antIcon = <LoadingOutlined style={{ fontSize: 64 }} spin />;
+  return (
+    <>
+      <div className="centerAbsolute">
+        <Spin indicator={antIcon}></Spin>
+      </div>
+    </>
+  );
+}
+
+export default PageLoader;
